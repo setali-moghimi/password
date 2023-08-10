9c53671912755aa80196935b38f02e1e76ebb392
unknown <setalim7@gmail.com>
2023-08-10 15:22:21 +0330

    upload mini project in github

---
+++ source/js/script.js
@@ -0,0 +1,26 @@
+﻿ // Functions for select elems with id & class
+ var $ = document;
+ function _id(id_name) {
+   return $.getElementById(id_name);
+ }
+ function _class(class_name) {
+   return $.getElementsByClassName(class_name);
+ }
+
+ // Select Elems
+ var togglePassword = _class("toggle-password");
+ var passwordField = _id("password-field");
+ console.log(togglePassword)
+ console.log(passwordField)
+
+ // Fire click event on eye icon
+ togglePassword[0].addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.setAttribute('type','password')
+     togglePassword[0].classList.remove("active");
+   } else {
+    passwordField.setAttribute('type','text')
+    togglePassword[0].classList.add("active");
+   }
+   console.log('g');
+ })
